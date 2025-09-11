cfa02b3676c4232f5961e2a5ea1912b690dbdeb9
hildirama <hildirama123@gmail.com>
2025-09-11 12:37:54 +0700

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import Hero from './components/hero';
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-5xl text-violet-300 font-bold">welcome to HIRASENA</h1>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
     </main>
   )
 }
