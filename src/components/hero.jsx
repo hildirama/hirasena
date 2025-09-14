c801d7c8167484eee5babf9dc22741991cdedd08
hildirama <hildirama123@gmail.com>
2025-09-14 15:51:12 +0700

    about section

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -4,7 +4,7 @@ import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
-import { ScrollTrigger } from "gsap/all"; gsap/
+import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger)
 
