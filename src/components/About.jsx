5a66a4a02974d971343a2457804d63fb4a9a2e02
hildirama <hildirama123@gmail.com>
2025-10-04 17:57:37 +0700

    navbar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -33,8 +33,7 @@ const About = () => {
                 md:text-[10px]'>welcome to hirasena
                 </h2>
 
-                <AnimatedTitle title="Dis<b>o</b>ver the world's l<b>a</b>rgest shared
-                    adventure" containerClass="mt-5 !text-black text-center" />
+                <AnimatedTitle title="Dis<b>o</b>ver the world's <br /> l<b>a</b>rgest shared adventure" containerClass="mt-5 !text-black text-center" />
 
 
                 <div className='about-subtext'>
--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,8 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+
 
 const AnimatedTitle = ({title, containerClass}) => {
 
-    const containerRef = useRef(null)
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const ctx = gsap.context(() => {
+            const titleAnimation = gsap.timeline({
+                scrollTrigger: {
+                    trigger: containerRef.current,
+                    start: '100 bottom',
+                    end: 'center-bottom',
+                    toggleActions: 'play none none reverse'
+                }
+            });
+            titleAnimation.to('.animated-word', {
+                opacity: 1,
+                transform: 'translate3d(0,0,0) rotateY(0deg) rotateX(0deg)',
+                ease: 'power2.inOut',
+                stagger: 0.02,
+            })
+        }, containerRef)
+        return() => ctx.revert();
+    }, [])
 
     return (
                 <div 
